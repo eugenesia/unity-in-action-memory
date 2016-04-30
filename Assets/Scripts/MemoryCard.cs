ba6dd86f991400892eadb28fdc769c29e134ead7
Eugene Sia <eugene.sia@ticketmaster.co.uk>
2016-04-30 04:09:27 +0100

    Reveal card on click sect 5.2.3

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -4,17 +4,23 @@ using System.Collections;
 // React to clicks by turning over.
 public class MemoryCard : MonoBehaviour {
 
+	// Sprite representing back of the card.
+	[SerializeField] private GameObject cardBack;
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	public void OnMouseDown() {
-		Debug.Log("testing 1 2 3");
+
+		// Only deactivate if object is currently active/visible.
+		if (cardBack.activeSelf) {
+			// Set object inactive/invisible.
+			cardBack.SetActive(false);
+		}
 	}
 }
