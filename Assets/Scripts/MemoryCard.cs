c67a1ebffcdd42b1c737c03a15099fa3dacd8e12
Eugene Sia <eugene.sia@ticketmaster.co.uk>
2016-04-30 04:02:50 +0100

    React to clicks sect 5.2.2

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// React to clicks by turning over.
+public class MemoryCard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnMouseDown() {
+		Debug.Log("testing 1 2 3");
+	}
+}
