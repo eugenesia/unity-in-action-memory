5fbfb6ec726293cf889820823c0acd8cca52e838
Eugene Sia <eugene@eugenesia.co.uk>
2016-05-07 00:51:35 +0100

    Instantiate a grid of cards sect 5.3.3

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,6 +4,14 @@ using System.Collections;
 // Scene controller to coordinate card actions.
 public class SceneController : MonoBehaviour {
 
+	// Grid to place cloned cards.
+	public const int gridRows = 2;
+	public const int gridCols = 4;
+
+	// Spacing between cards.
+	public const float offsetX = 2f;
+	public const float offsetY = 2.5f;
+
 	// Reference for the original card in the scene, to be cloned.
 	[SerializeField] private MemoryCard originalCard;
 
@@ -13,10 +21,37 @@ public class SceneController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		int id = Random.Range(0, images.Length);
-		// Set the original card's image asset to a random one, and set
-		// its ID to correspond to the selected image.
-		originalCard.SetCard(id, images[id]);
+
+		Vector3 startPos = originalCard.transform.position;
+
+		// Clone the original card and place it on the grid.
+		for (int i = 0; i < gridCols; i++) {
+			for (int j = 0; j < gridRows; j++) {
+
+				MemoryCard card;
+
+				if (i == 0 && j == 0) {
+					card = originalCard;
+				} else {
+
+					// Clone the Memory Card GameObject by cloning its attached Component.
+					// If you are cloning a Component, then the GameObject it
+					// is attached to will also be cloned.
+					// http://docs.unity3d.com/ScriptReference/Object.Instantiate.html
+					card = Instantiate(originalCard) as MemoryCard;
+				}
+
+				int id = Random.Range(0, images.Length);
+				// Set the card's image asset to a random one, and set
+				// its ID to correspond to the selected image.
+				card.SetCard(id, images[id]);
+
+				// Place cloned card in the grid.
+				float posX = (offsetX * i) + startPos.x;
+				float posY = -(offsetY * j) + startPos.y;
+				card.transform.position = new Vector3(posX, posY, startPos.z);
+			}
+		}
 	}
 	
 	// Update is called once per frame
